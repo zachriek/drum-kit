343b2bc8e1181038404174eee8f35dee76aae599
Muhammad Zachrie Kurniawan <zachrie2005@gmail.com>
2021-06-17 13:41:00 +0700

    🔥

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function removeTransition(e) {
+	if (e.propertyName !== 'transform') return;
+	e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+	if (!audio) return;
+
+	key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
+
+for (let i = 0; i < keys.length; i++ ) {
+	keys[i].addEventListener('click', function(e) {
+		const audio = document.querySelector(`audio.audio-${i+1}`);
+		if (!audio) return;
+
+		keys[i].classList.add('playing');
+		audio.currentTime = 0;
+		audio.play();
+	});
+}
